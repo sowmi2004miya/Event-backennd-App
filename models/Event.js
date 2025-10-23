72ea915d78aa57b7bd620373ee79eb6a83377aa6
sowmi2004miya <g.sowmiya2004@gmail.com>
2025-10-23 17:46:39 +0530

    first commit

---
+++ models/Event.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  date: { type: String, required: true },
+  organiser: { type: String, required: true },
+  location: { type: String, required: true },
+});
+
+module.exports= mongoose.model("Event", eventSchema);
+
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const eventRoutes = require('./routes/eventRoutes');
+require('dotenv').config();
+const cors = require('cors');
+const PORT = 5000; 
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URI)
+ .then(() => console.log('MongoDB Connected!'))
+ .catch((err) => {
+   console.error('MongoDB Connection Error!!',err.message);
+ });
+
+ app.use('/api/occasion',eventRoutes);
+
+ app.listen(PORT,() =>{
+    console.log(`Server is running on port${PORT}`);
+ });
